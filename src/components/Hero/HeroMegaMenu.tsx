632a23fc54c393446c85a2fbf4fe5cde2851a0ac
Marcos Daniel Romero Garcia <Marck Romero>
2025-05-30 00:08:29 -0500

    Mega menu esqueleto armado v2

--- src/components/Hero/HeroMegaMenu.tsx
+++ src/components/Hero/HeroMegaMenu.tsx
@@ -9,8 +9,8 @@ export const HeroMegaMenu = () => {
           <li className="relative group">
             <IoMenuSharp className="w-10 h-10 text-white cursor-pointer" />
             <div className="absolute top-0 left-0 transition  translate-x-0 opacity-0 invisible group-hover:opacity-100 group-hover:visible duration-500 ease-in-out group-hover:transform z-50 min-w-[200px] transform">
-              <ul className="relative left-0 border py-2 px-4 top-14 bg-white shadow-xl">
-                <li className="group/sub">
+              <ul className="relative left-0 border top-14 bg-white shadow-xl">
+                <li className="group/sub p-4">
                   <span className="cursor-pointer">Primera opción</span>
                   <div className="absolute left-full top-0 opacity-0 invisible group-hover/sub:opacity-100 group-hover/sub:visible transition-all duration-300">
                     <div className="relative bg-white border shadow-xl min-w-[200px]">
@@ -31,15 +31,15 @@ export const HeroMegaMenu = () => {
                     </div>
                   </div>
                 </li>
-                <li className="group/sub">
+                <li className="group/sub p-4">
                   <span className="cursor-pointer">Primera opción</span>
                   <div className="absolute left-full top-0 opacity-0 invisible group-hover/sub:opacity-100 group-hover/sub:visible transition-all duration-300">
                     <div className="relative bg-white border shadow-xl min-w-[200px]">
                       <div className="flex">
                         <ul className="min-w-[200px]">
-                          <li className="hover:bg-gray-100 py-2 px-4">Subopción 1</li>
-                          <li className="hover:bg-gray-100 py-2 px-4">Subopción 2</li>
-                          <li className="hover:bg-gray-100 py-2 px-4">Subopción 3</li>
+                          <li className="hover:bg-gray-100 py-2 px-4">Subopción 4</li>
+                          <li className="hover:bg-gray-100 py-2 px-4">Subopción 5</li>
+                          <li className="hover:bg-gray-100 py-2 px-4">Subopción 6</li>
                         </ul>
                         <Image
                           src="https://placehold.co/600x400"
